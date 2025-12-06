ae1ede2df38dd92aab6517d117c5d5126e49d4dd
“Cx330-Paulownia” <Cx330t_Personal@163.com>
2025-12-07 00:29:31 +0800

    fonts: Update fonts to use truetype versions

--- src/config.ts
+++ src/config.ts
@@ -186,14 +186,14 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "LXGWWenKaiScreen.ttf",
+			fontFamily: "ZenMaruGothic-Medium",
 			fontWeight: "400",
-			localFonts: ["LXGWWenKaiScreen.ttf"],
+			localFonts: ["ZenMaruGothic-Medium.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "LXGWWenKaiScreen.ttf",
+			fontFamily: "LXGWWenKaiScreen",
 			fontWeight: "500",
 			localFonts: ["LXGWWenKaiScreen.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
